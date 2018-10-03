1c62b6a1be355d21f97d6fa86a6262172be1e615
SiamKing <thesiameseelectric@gmail.com>
2018-10-03 16:59:28 -0700

    Added route to play trivia

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { SetupComponent } from './setup/setup.component';
+import { PlayComponent } from './play/play.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'setup', component: SetupComponent }
+  { path: 'play', component: PlayComponent }
 ];
 
 @NgModule({
