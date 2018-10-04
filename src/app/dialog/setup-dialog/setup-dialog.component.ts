e0e4a813eb46febc59daf465608dc46c5258a2f1
SiamKing <thesiameseelectric@gmail.com>
2018-10-03 17:03:14 -0700

    Set up dialog for configuring trivia game - category and difficulty

--- src/app/dialog/setup-dialog/setup-dialog.component.ts
+++ src/app/dialog/setup-dialog/setup-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { NgForm } from '@angular/forms';
+import { DataService } from '../../data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-setup-dialog',
@@ -11,7 +13,9 @@ export class SetupDialogComponent {
 
 
   constructor(public dialogRef: MatDialogRef<any>,
-              @Inject(MAT_DIALOG_DATA) public data: any) { }
+              @Inject(MAT_DIALOG_DATA) public data: any,
+              private dataService: DataService,
+              private router: Router) { }
 
   onNoClick(): void {
     console.log('fat');
@@ -19,7 +23,9 @@ export class SetupDialogComponent {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form);
+    this.dataService.setValues(form.value.category, form.value.difficulty);
+    this.dialogRef.close();
+    this.router.navigate(['/play']);
   }
 
 }
