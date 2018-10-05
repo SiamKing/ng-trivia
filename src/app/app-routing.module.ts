cfff6b5cf8622017cbefaf5952e5cc425aafdfd1
SiamKing <thesiameseelectric@gmail.com>
2018-10-05 13:31:53 -0700

    Added link and route for Instructions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PlayComponent } from './play/play.component';
+import { InstructionsComponent } from './instructions/instructions.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'play', component: PlayComponent }
+  { path: 'play', component: PlayComponent },
+  { path: 'instructions', component: InstructionsComponent }
 ];
 
 @NgModule({
