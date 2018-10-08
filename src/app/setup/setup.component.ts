20a2ac937bbf56f7a8ffea825b80995b5829ea92
SiamKing <thesiameseelectric@gmail.com>
2018-10-07 20:53:26 -0700

    Added setup component and refactored home component and moved setup dialog

---
+++ src/app/setup/setup.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+
+import { SetupDialogComponent } from '../dialog/setup-dialog/setup-dialog.component';
+import { categories, difficulties } from '../data.model';
+
+@Component({
+  selector: 'app-setup',
+  templateUrl: './setup.component.html',
+  styleUrls: ['./setup.component.css']
+})
+export class SetupComponent implements OnInit {
+  category: string;
+  difficulty: string;
+  categories = categories;
+  difficulties = difficulties;
+
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.openDialog();
+    }, 500)
+    
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(SetupDialogComponent, {
+      width: '250px',
+      data: {
+        categories: this.categories,
+        difficulties: this.difficulties
+      }
+    })
+
+    dialogRef.afterClosed().subscribe(res => {
+        console.log(res);
+    });
+  }
+
+}
