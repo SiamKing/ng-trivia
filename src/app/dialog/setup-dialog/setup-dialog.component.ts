20ddc02b4a0c31102b6d08bcc4f9ec5040e39631
SiamKing <thesiameseelectric@gmail.com>
2018-10-08 14:37:55 -0700

    Added functionality for cancel button

--- src/app/dialog/setup-dialog/setup-dialog.component.ts
+++ src/app/dialog/setup-dialog/setup-dialog.component.ts
@@ -1,8 +1,9 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { NgForm } from '@angular/forms';
-import { DataService } from '../../data.service';
 import { Router } from '@angular/router';
+
+import { DataService } from '../../data.service';
 
 @Component({
   selector: 'app-setup-dialog',
@@ -15,17 +16,24 @@ export class SetupDialogComponent {
   constructor(public dialogRef: MatDialogRef<any>,
               @Inject(MAT_DIALOG_DATA) public data: any,
               private dataService: DataService,
-              private router: Router) { }
+              private router: Router) {
+    dialogRef.disableClose = true
+  }
 
   onNoClick(): void {
-    console.log('fat');
-    this.dialogRef.close();
+    this.onCancel();
   }
 
   onSubmit(form: NgForm) {
     this.dataService.setValues(form.value.category, form.value.difficulty);
     this.dialogRef.close();
     this.router.navigate(['/play']);
+  }
+
+  onCancel() {
+    console.log('object');
+    this.dialogRef.close();
+    this.router.navigate(['/']);
   }
 
 }
