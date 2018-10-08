b6b5056a683c81e9fc5e85dcb8a86ec1402f5f54
SiamKing <thesiameseelectric@gmail.com>
2018-10-08 14:29:04 -0700

    Refactored and put relevant code in Setup component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,45 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material';
-
-import { SetupDialogComponent } from '../dialog/setup-dialog/setup-dialog.component';
-import { categories } from '../data.model';
-import { difficulties } from '../data.model';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  welcome = true;
-  category: string;
-  difficulty: string;
-  categories = categories;
-  difficulties = difficulties;
 
+  constructor() { }
 
-  constructor(public dialog: MatDialog) { }
-
-  ngOnInit() {
-  }
-
-  openDialog(): void {
-    this.welcome = false;
-    const dialogRef = this.dialog.open(SetupDialogComponent, {
-      width: '250px',
-      data: {
-        categories: this.categories,
-        difficulties: this.difficulties
-      }
-    })
-
-    dialogRef.afterClosed().subscribe(res => {
-      if (res == undefined) {
-        this.welcome = true;
-      } else {
-        console.log(res);
-      }
-    })
-  }
+  ngOnInit() { }
 
 }
