c376c174accda9fda159d1d2a28f5f2b55cf7e19
SiamKing <thesiameseelectric@gmail.com>
2018-10-08 14:19:52 -0700

    Added route for setup component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PlayComponent } from './play/play.component';
 import { InstructionsComponent } from './instructions/instructions.component';
+import { SetupComponent } from './setup/setup.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'setup', component: SetupComponent },
   { path: 'play', component: PlayComponent },
   { path: 'instructions', component: InstructionsComponent }
 ];
