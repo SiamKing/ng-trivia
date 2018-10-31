ec0b9929951286cf82b8dd77cc057c6ce63e3ce7
SiamKing <thesiameseelectric@gmail.com>
2018-10-30 19:49:09 -0700

    Added routing for signup and login components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { HomeComponent } from './home/home.component';
 import { PlayComponent } from './play/play.component';
 import { InstructionsComponent } from './instructions/instructions.component';
 import { SetupComponent } from './setup/setup.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'setup', component: SetupComponent },
   { path: 'play', component: PlayComponent },
-  { path: 'instructions', component: InstructionsComponent }
+  { path: 'instructions', component: InstructionsComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
