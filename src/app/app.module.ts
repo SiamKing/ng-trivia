0fe887cdeaa193c1dbca2aa2af7282e9228dd60a
SiamKing <thesiameseelectric@gmail.com>
2018-10-30 19:32:48 -0700

    Updated firebase version and added FireAuth modules to main app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,8 +16,6 @@ import { NavbarComponent } from './navigation/navbar/navbar.component';
 import { SidenavComponent } from './navigation/sidenav/sidenav.component';
 import { HomeComponent } from './home/home.component';
 import { SetupDialogComponent } from './dialog/setup-dialog/setup-dialog.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { PlayComponent } from './play/play.component';
 import { QuestionDialogComponent } from './dialog/question-dialog/question-dialog.component';
 import { HtmlDecoderPipe } from './shared/html-decoder.pipe';
@@ -20,6 +23,8 @@ import { ScoreDialogComponent } from './dialog/score-dialog/score-dialog.compone
 import { InstructionsComponent } from './instructions/instructions.component';
 import { environment } from '../environments/environment';
 import { SetupComponent } from './setup/setup.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
@@ -34,6 +39,8 @@ import { SetupComponent } from './setup/setup.component';
     ScoreDialogComponent,
     InstructionsComponent,
     SetupComponent,
+    SignupComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -43,7 +50,10 @@ import { SetupComponent } from './setup/setup.component';
     FlexLayoutModule,
     FormsModule,
     HttpClientModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent],
