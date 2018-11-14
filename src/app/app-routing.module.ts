32debda4aa3d22825e38bb80a582a41989ef9c11
SiamKing <thesiameseelectric@gmail.com>
2018-11-13 19:19:24 -0800

    Added user path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { InstructionsComponent } from './instructions/instructions.component';
 import { SetupComponent } from './setup/setup.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
+import { UserComponent } from './user/user.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -13,7 +15,8 @@ const routes: Routes = [
   { path: 'play', component: PlayComponent },
   { path: 'instructions', component: InstructionsComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'user', component: UserComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
