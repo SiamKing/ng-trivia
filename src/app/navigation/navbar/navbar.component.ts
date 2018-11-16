052903b241e3ae830a28599d857f2be442757a1d
SiamKing <thesiameseelectric@gmail.com>
2018-11-16 12:37:24 -0800

    Added links for logging in and out and user profile along with functionalities

--- src/app/navigation/navbar/navbar.component.ts
+++ src/app/navigation/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/shared/ui.service';
 
 
 @Component({
@@ -6,16 +9,35 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
   @Output() sidenavToggle = new EventEmitter<void>();
+  isAuth = false;
+  authSubscription: Subscription;
+  displayNav = true;
 
-  constructor() { }
+  constructor(private authService: AuthService,
+              private uiService: UiService) { }
 
   ngOnInit() {
+    this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    })
+    this.uiService.navDisplay.subscribe(navDisplay => {
+      console.log(navDisplay);
+      this.displayNav = navDisplay;
+    })
   }
 
   onToggleSidenav() {
     this.sidenavToggle.emit();
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
   }
 
 }
--- src/app/navigation/sidenav/sidenav.component.ts
+++ src/app/navigation/sidenav/sidenav.component.ts
@@ -1,19 +1,35 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css']
 })
-export class SidenavComponent implements OnInit {
+export class SidenavComponent implements OnInit, OnDestroy {
   @Output() sidenavClose = new EventEmitter<void>();
+  isAuth = false;
+  authSubscription: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    })
   }
 
   onSidenavClose() {
     this.sidenavClose.emit();
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
   }
 }
