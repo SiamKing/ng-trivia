bc2e6edecc812fcb2a1379e79570159839445d34
SiamKing <thesiameseelectric@gmail.com>
2018-11-16 12:44:41 -0800

    Added Subjects for Quit game and NavDisplay so they can be subscribed to

--- src/app/shared/ui.service.ts
+++ src/app/shared/ui.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UiService {
+  quitter = new Subject<boolean>();
+  navDisplay = new Subject<boolean>();
 
   constructor(private snackbar: MatSnackBar) { }
 
   showSnackBar(message, action, config) {
     this.snackbar.open(message, action, config);
+  }
+
+  quitGame() {
+    this.quitter.next(true);
+  }
+
+  navDisplayShow() {
+    this.navDisplay.next(true);
+  }
+
+  navDisplayHide() {
+    this.navDisplay.next(false);
   }
 }
