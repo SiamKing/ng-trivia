ba817b6b9e41cc1890bfb9bd95f872e3b2efb39f
SiamKing <thesiameseelectric@gmail.com>
2018-11-16 12:40:49 -0800

    Added card for profile info and table for past trivia challenge results. Added dummy data in component for table

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TriviaResult } from '../shared/trivia-result.model';
 
 @Component({
   selector: 'app-user',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  dataSource = games;
+  triviaColumns = ['date', 'category', 'difficulty', 'correctAnswers', 'score'];
 
   constructor() { }
 
@@ -13,3 +16,20 @@ export class UserComponent implements OnInit {
   }
 
 }
+
+const games: TriviaResult[] = [
+  {
+    date: "11/21/18",
+    category: "TV",
+    difficulty: "easy",
+    correctAnswers: 9,
+    score: 380
+  },
+  {
+    date: "11/22/18",
+    category: "politics",
+    difficulty: "medium",
+    correctAnswers: 7,
+    score: 460
+  }
+]
