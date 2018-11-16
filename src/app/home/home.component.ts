516dfa4f05b95f9ea4b6e18bfce64dc59462ed60
SiamKing <thesiameseelectric@gmail.com>
2018-11-16 12:33:58 -0800

    Injected UiService to change nav display

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from '../shared/ui.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private uiService: UiService) { }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    this.uiService.navDisplayShow();
+  }
 
 }
