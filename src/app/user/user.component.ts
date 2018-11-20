a19de553597a8c91e32e6fab8ebee5970c437d05
SiamKing <thesiameseelectric@gmail.com>
2018-11-20 13:36:25 -0800

    Subscribed to getUser function for displayName and email

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,17 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { TriviaResult } from '../shared/trivia-result.model';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
-  
+export class UserComponent implements OnInit, OnDestroy {
+  user;
+  userSubscription = new Subscription();
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.userSubscription = this.authService.getUser().subscribe(user => {
+      if (user) {
+        this.user = user;
+      } else {
+        this.user = null;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    console.log("unsubscribe to userSub");
+    this.userSubscription.unsubscribe();
+    this.user = null;
   }
 
 }
