f246c0333fd40613ff15a51ab0d35cbc8e1e3148
SiamKing <thesiameseelectric@gmail.com>
2018-11-20 13:11:10 -0800

    Updated functionality for fetching past challenges to use data from db instead of dummy data

--- src/app/user/past-challenges/past-challenges.component.ts
+++ src/app/user/past-challenges/past-challenges.component.ts
@@ -1,42 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { TriviaResult } from 'src/app/shared/trivia-result.model';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+
+import { TriviaResultsService } from '../trivia-results.service';
+import { TriviaResults } from 'src/app/shared/trivia-results.model';
+import { Subscription } from 'rxjs';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { map } from 'rxjs/operators';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-past-challenges',
   templateUrl: './past-challenges.component.html',
   styleUrls: ['./past-challenges.component.css']
 })
-export class PastChallengesComponent implements OnInit {
-  dataSource = games;
-  // dataSource: Observable<any>;
-  triviaColumns = ['date', 'category', 'difficulty', 'correctAnswers', 'score'];
+export class PastChallengesComponent implements OnInit, OnDestroy {
+  // dataSource = new MatTableDataSource<TriviaResults>();
+  dataSource;
+  totalScore
+  triviaResultsSubscription: Subscription;
+  triviaColumns = ['date', 'category', 'difficulty', 'correctAnswers', 'score', 'completed'];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator
   
-  constructor(private db: AngularFirestore) { }
+  constructor(private tRService: TriviaResultsService, private db: AngularFirestore) { }
 
   ngOnInit() {
-    // this.dataSource = this.db
-    //   .collection('past-challenges')
-    //   .snapshotChanges()
-    //   .subscribe(res => console.log(res))
-  }
+    const userId = localStorage.getItem('userUID');
+    // 
+    this.tRService.fetchPastChallenges()
+      .subscribe(res => {
+        this.dataSource = res
+        this.totalScore = this.dataSource.reduce((sum, data) => sum + data.score )
+        console.log(res)
+      })
+)
+    // this.dataSource = new MatTableDataSource<TriviaResults>(this.tRService.fetchPastChallenges());
+    // this.dataSource.paginator = this.paginator;
+   }
+
+   ngOnDestroy() {
+    //  this.triviaResultsSubscription.unsubscribe();
+   }
 
 }
-
-const games: TriviaResult[] = [
-  {
-    date: "11/21/18",
-    category: "TV",
-    difficulty: "easy",
-    correctAnswers: 9,
-    score: 380
-  },
-  {
-    date: "11/22/18",
-    category: "politics",
-    difficulty: "medium",
-    correctAnswers: 7,
-    score: 460
-  }
-]
